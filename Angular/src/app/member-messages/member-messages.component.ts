742d40dcca27d5ae2ba20553e529851efd5dc5b9
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-03-31 05:48:00 +0400

    feat: added sending messages logic into client

--- Angular/src/app/member-messages/member-messages.component.ts
+++ Angular/src/app/member-messages/member-messages.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Message} from '../_models/message';
 import {MessageService} from "../_services/message.service";
 import {MembersService} from "../_services/members.service";
 import {ActivatedRoute} from "@angular/router";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-member-messages',
@@ -11,12 +12,13 @@ import {ActivatedRoute} from "@angular/router";
     '../../assets/css/tailwind.css', '../../assets/css/uikit.css']
 })
 export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm: NgForm;
   @Input() username: string;
   messages: Message[];
+  messageContent: string;
 
 
-  constructor(private messageService: MessageService, private memberService: MembersService,
-              private route: ActivatedRoute) {
+  constructor(private messageService: MessageService, private memberService: MembersService) {
   }
 
   ngOnInit(): void {
@@ -26,6 +28,14 @@ export class MemberMessagesComponent implements OnInit {
   loadMessages() {
     this.messageService.getMessageThread(this.username).subscribe(messages => {
       this.messages = messages;
+    })
+  }
+
+  sendMessage() {
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe(message => {
+      this.messages.push(message);
+      console.log(message);
+      this.messageForm.reset();
     })
   }
 
