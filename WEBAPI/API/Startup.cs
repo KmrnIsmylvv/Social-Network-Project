5fb0246716e4919e194161812b251d3ea25529b9
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-04-05 11:00:51 +0400

    feat: adding Message Hub into API

--- WEBAPI/API/Startup.cs
+++ WEBAPI/API/Startup.cs
@@ -79,6 +79,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<PresenceHub>("hubs/presence");
+                endpoints.MapHub<MessageHub>("hubs/message");
             });
         }
     }
---
+++ WEBAPI/BLL/SignalR/MessageHub.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using BLL.Helpers.Extensions;
+using BLL.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BLL.SignalR
+{
+    public class MessageHub : Hub
+    {
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+
+        public MessageHub(IMessageRepository messageRepository, IMapper mapper)
+        {
+            _messageRepository = messageRepository;
+            _mapper = mapper;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var otherUser = httpContext.Request.Query["user"].ToString();
+            var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            var messages = await _messageRepository
+                .GetMessageThread(Context.User.GetUsername(), otherUser);
+
+            await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        private string GetGroupName(string caller, string other)
+        {
+            var stringCompare = string.CompareOrdinal(caller, other) < 0;
+            return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+        }
+    }
+}
