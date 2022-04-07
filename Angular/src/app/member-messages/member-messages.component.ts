5db5138fe137c4675d7fa3f34ef8bfc5138777e2
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-04-07 22:14:29 +0400

    refactor:Tidying up the member message component

--- Angular/src/app/member-messages/member-messages.component.ts
+++ Angular/src/app/member-messages/member-messages.component.ts
@@ -1,15 +1,12 @@
-import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Message} from '../_models/message';
 import {MessageService} from "../_services/message.service";
 import {MembersService} from "../_services/members.service";
-import {ActivatedRoute} from "@angular/router";
 import {NgForm} from "@angular/forms";
-import {AccountService} from "../_services/account.service";
 import {User} from "../_models/user";
-import {take} from "rxjs";
-import {Member} from "../_models/member";
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
   styleUrls: ['../../assets/css/style.css', '../../assets/css/icons.css',
