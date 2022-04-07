e41c56d93fbc42bf494e9dffc6db42f816004729
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-04-07 18:07:46 +0400

    refactor: Optimizing queries

--- WEBAPI/BLL/Services/Repositories/MessageRepository.cs
+++ WEBAPI/BLL/Services/Repositories/MessageRepository.cs
@@ -76,28 +76,25 @@ namespace BLL.Services.Repositories
         {
             var query = _context.Messages
                 .OrderByDescending(m => m.MessageSent)
+                .ProjectTo<MessageDto>(_mapper.ConfigurationProvider)
                 .AsQueryable();
 
             query = messageParams.Container switch
             {
                 "Inbox" => query.Where(u =>
-                    u.Recipient.UserName == messageParams.Username && u.RecipientDeleted == false),
+                    u.RecipientUsername == messageParams.Username && u.RecipientDeleted == false),
                 "Outbox" => query.Where(u =>
-                    u.Sender.UserName == messageParams.Username && u.SenderDeleted == false),
+                    u.SenderUsername == messageParams.Username && u.SenderDeleted == false),
                 _ => query.Where(u =>
-                    u.Recipient.UserName == messageParams.Username && u.RecipientDeleted == false && u.DateRead == null)
+                    u.RecipientUsername == messageParams.Username && u.RecipientDeleted == false && u.DateRead == null)
             };
 
-            var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
-
-            return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
+            return await PagedList<MessageDto>.CreateAsync(query, messageParams.PageNumber, messageParams.PageSize);
         }
 
         public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername)
         {
             var messages = await _context.Messages
-                .Include(u => u.Sender).ThenInclude(p => p.Photos)
-                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
                 .Where(m =>
                     m.Recipient.UserName == currentUsername && m.RecipientDeleted == false
                                                             && m.Sender.UserName == recipientUsername
@@ -105,10 +102,11 @@ namespace BLL.Services.Repositories
                     && m.Sender.UserName == currentUsername && m.SenderDeleted == false
                 )
                 .OrderBy(m => m.MessageSent)
+                .ProjectTo<MessageDto>(_mapper.ConfigurationProvider)
                 .ToListAsync();
 
             var unreadMessages = messages
-                .Where(m => m.DateRead == null && m.Recipient.UserName == currentUsername).ToList();
+                .Where(m => m.DateRead == null && m.RecipientUsername == currentUsername).ToList();
 
             if (unreadMessages.Any())
             {
@@ -118,7 +116,7 @@ namespace BLL.Services.Repositories
                 }
             }
 
-            return _mapper.Map<IEnumerable<MessageDto>>(messages);
+            return messages;
         }
     }
 }
